f79fe9c09ee6a499b32e1ea63caa5b3c84544c4e
avizoetic <avi.zoetic@gmail.com>
2020-09-08 13:42:01 +0545

    food app UI

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF5252);
+const ksecondaryColor = Color(0xFFB5BFD0);
+const kTextColor = Color(0xFF50505D);
+const kTextLightColor = Color(0xFF6A727D);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
---
+++ lib/screens/home/components/discount_card.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_app/constants.dart';
+import 'package:food_app/screens/details/details-screen.dart';
+import 'package:food_app/screens/home/components/item_card.dart';
+
+class DiscountCard extends StatelessWidget {
+  const DiscountCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Recommended",
+            style: TextStyle(fontWeight: FontWeight.bold, color: kTextColor),
+          ),
+          // Container(
+          //   margin: EdgeInsets.symmetric(vertical: 20),
+          //   width: double.infinity,
+          //   height: 166,
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(10),
+          //     image: DecorationImage(
+          //       fit: BoxFit.fill,
+          //       image: AssetImage("assets/images/beyond-meat-mcdonalds.png"),
+          //     ),
+          //   ),
+          //   child: DecoratedBox(
+          //     decoration: BoxDecoration(
+          //       borderRadius: BorderRadius.circular(10),
+          //       gradient: LinearGradient(
+          //         colors: [
+          //           Color(0xFFFF961F).withOpacity(0.7),
+          //           kPrimaryColor.withOpacity(0.7),
+          //         ],
+          //       ),
+          //     ),
+          //     child: Padding(
+          //       padding: const EdgeInsets.all(20.0),
+          //       child: Row(
+          //         children: <Widget>[
+          //           Expanded(
+          //             child: SvgPicture.asset("assets/icons/macdonalds.svg"),
+          //           ),
+          //           Expanded(
+          //             child: RichText(
+          //               text: TextSpan(
+          //                 style: TextStyle(color: Colors.white),
+          //                 children: [
+          //                   TextSpan(
+          //                     text: "Get Discount of \n",
+          //                     style: TextStyle(fontSize: 16),
+          //                   ),
+          //                   TextSpan(
+          //                     text: "30% \n",
+          //                     style: TextStyle(
+          //                       fontSize: 43,
+          //                       fontWeight: FontWeight.bold,
+          //                     ),
+          //                   ),
+          //                   TextSpan(
+          //                     text:
+          //                         "at MacDonald's on your first order & Instant cashback",
+          //                     style: TextStyle(fontSize: 10),
+          //                   ),
+          //                 ],
+          //               ),
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+                ItemCard(
+                  svgSrc: "assets/icons/burger_beer.svg",
+                  title: "Burger & Beer",
+                  shopName: "MacDonald's",
+                  press: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return DetailsScreen();
+                        },
+                      ),
+                    );
+                  },
+                ),
+                ItemCard(
+                  svgSrc: "assets/icons/chinese_noodles.svg",
+                  title: "Chinese & Noodles",
+                  shopName: "Wendys",
+                  press: () {},
+                ),
+                ItemCard(
+                  svgSrc: "assets/icons/burger_beer.svg",
+                  title: "Burger & Beer",
+                  shopName: "MacDonald's",
+                  press: () {},
+                )
+              ],
+            ),
+          )
+
+          // ),
+        ],
+      ),
+    );
+  }
+}
